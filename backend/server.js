62138ae7650afce8adcae08ded21ff67d2dbdd1f
Anupam2000 <anupamsahoo2000@gmail.com>
2025-10-03 22:35:30 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const app = express();
 const db = require("./utils/db");
 const userRoutes = require("./routes/user");
+const cors = require("cors");
+app.use(cors());
 
-const app = express();
 app.use(express.json());
 
 // Routes
